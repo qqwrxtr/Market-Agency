d14c9204c86108a50e01336686ea5d3ebe288360
qqwrxtr <teodorghirba1@gmail.com>
2024-09-15 23:06:59 +0300

    enabled analytics for vercel

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/store.jsx';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App cardInfo={store.cardInfo} steps={store.steps} history={store.history} testimonials={store.testimonials}/>
+    <Analytics/>
   </React.StrictMode>
 );
 
